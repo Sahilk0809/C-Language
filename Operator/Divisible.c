5c7d55010e142e472620898a6157386328144519
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2023-11-01 15:27:11 +0530

    Add files via upload

---
+++ Operator/Divisible.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main()
+{
+	
+	int n;
+	
+	printf("Enter The Value :");
+	scanf("%d",&n);
+	
+	if(n%5==0 && n%11==0)
+	{
+		printf("The Value Is Divisible by 5 and 11");
+	}
+	else
+	{
+		printf("The Value is Not Divisible by 5 and 11");
+	}
+}
---
+++ Operator/Logical Operator.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+main() 
+{
+	
+    int a = 5;
+    int b = 10;
+    int c = 15;
+
+    // Logical AND operator
+    if(a > 0 && b > 0 && c > 0) 
+	{
+        printf("All numbers are greater than 0\n");
+    }
+    
+    // Logical OR operator
+    if(a > 0 || b > 0 || c > 0) 
+	{
+        printf("At least one number is greater than 0\n");
+    }
+
+    // Logical NOT operator
+    if(!(a < 0)) 
+	{
+        printf("A is not greater than 0\n");
+    }
+
+}
