a01104bb20446f44459dc6e1beb5c2df07f26761
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2023-11-03 16:23:58 +0530

    Add files via upload

---
+++ Greenhorns/Pattern2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+main()
+{
+    printf("    *   *   *   *   *   *   *\n\n");
+    printf("*   *   *   *   *   *   *   *\n");
+    printf("*       *                   *\n");
+    printf("*       *       *   *   *   *\n");
+    printf("*       *       *   *   *   *\n");
+    printf("*   *   *   *   *   *   *   *");
+
+}
