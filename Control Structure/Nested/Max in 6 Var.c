26faac8dba48b99e0fe67f26c3887d5910579758
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2023-11-06 18:50:32 +0530

    Update Max in 6 Var.c

--- Control Structure/Nested/Max in 6 Var.c
+++ Control Structure/Nested/Max in 6 Var.c
@@ -7,54 +7,71 @@ main()
    printf("Enter The Values Of a,b,c,d,e,f : ");
    scanf("%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f);
 
+   //a,b,c,d,e,f
    if (a > b) 
    {
+      //a,c,d,e,f
       if (a > c) 
       {
+         //a,d,e,f
          if (a > d) 
          {
+            //a,e,f
             if (a > e) 
             {
+               //a,f
                if (a > f) 
                {
+                  //a
                   printf("A is Max");
                } 
                else 
                {
+                  //f
                   printf("F is Max");
                }
             } 
             else 
             {
+               //e,f
                if (e > f) 
                {
+                  //e
                   printf("E is Max");
                } else 
                {
+                  //f
                   printf("F is Max");
                }
             }
          } 
          else 
          {
+            //d,e,f
             if (d > e) 
             {
+               //d,f
                if (d > f) 
                {
+                  //d
                   printf("D is Max");
                } else 
                {
+                  //f
                   printf("F is Max");
                }
             } 
             else 
             {
+               //e,f
                if (e > f) 
                {
+                   //e
                    printf("E is Max");
                } 
                else 
                {
+                   //f
                    printf("F is Max");
                }
             }
@@ -62,52 +79,67 @@ main()
       } 
       else 
       {
+         //c,d,e,f
          if (c > d) 
          {
+            //c,e,f
             if (c > e) 
             {
+               //c,f
                if (c > f) 
                {
+                  //c
                   printf("C is Max");
                } 
                else 
                {
+                   //f
                    printf("F is Max");
                }
             } 
             else 
             {
+               //e,f
                if (e > f) 
                {
+                  //e
                   printf("E is Max");
                } 
                else 
                {
+                   //f
                    printf("F is Max");
                }
             }
          } 
          else 
          {
+            //d,e,f
             if (d > e) 
             {
+               //d,f
                if (d > f) 
                {
+                   //d
                    printf("D is Max");
                } 
                else 
                {
+                  //f
                   printf("F is Max");
                }
             } 
             else
             {
+               //e,f
                if (e > f) 
                {
+                  //e
                   printf("E is Max");
                } 
                else 
                {
+                  //f
                   printf("F is Max");
                }
             }
@@ -116,53 +148,69 @@ main()
    } 
    else 
    {
+      //b,c,d,e,f
       if (b > c) 
       {
+         //b,d,e,f
          if (b > d)
          {
+            //b,e,f
             if (b > e)
             {
+               //b,f
                if (b > f) 
                {
+                  //b
                   printf("B is Max");
                } 
                else 
                {
+                  //f
                   printf("F is Max");
                }
             } 
             else 
             {
+               //e,f
                if (e > f)
                {
+                   //e
                    printf("E is Max");
                } 
                else 
                {
+                   //f
                    printf("F is Max");
                }
             }
          } 
          else 
          {
+            //d,e,f
             if (d > e) 
             {
+               //d,f
                if (d > f) 
                {
+                   //d
                    printf("D is Max");
                } else 
                {
+                   //f
                    printf("F is Max");
                }
             }
             else 
             {
+               //e,f
                if (e > f) 
                {
+                  //e
                   printf("E is Max");
                } 
                else 
                {
+                  //f
                   printf("F is Max");
                }
             }
@@ -170,52 +218,67 @@ main()
       } 
       else 
       {
+         //c,d,e,f
          if (c > d) 
          {
+            //c,e,f
             if (c > e) 
             {
+               //c,f
                if (c > f) 
                {
+                   //c
                    printf("C is Max");
                }
                else 
                {
+                   //f
                    printf("F is Max");
                }
             } 
             else 
             {
+               //e,f
                if (e > f)
                {
+                   //e
                    printf("E is Max");
                } 
                else 
                {
+                   //f
                    printf("F is Max");
                }
             }
          }
          else 
          {
+            //d,e,f
             if (d > e) 
             {
+               //d,f
                if (d > f) 
                {
+                   //d
                    printf("D is Max");
                } 
                else
                {
+                   //f
                    printf("F is Max");
                }
             } 
             else
             {
+               //e,f
                if (e > f) 
                {
+                  //e
                   printf("E is Max");
                } 
                else 
                {
+                  //f
                   printf("F is Max");
                }
             }
