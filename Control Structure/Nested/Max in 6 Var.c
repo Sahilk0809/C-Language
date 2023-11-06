b2947d1e9e7add6b18cae484ca973025d2f7e1ef
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2023-11-06 18:29:01 +0530

    Update Max in 6 Var.c

--- Control Structure/Nested/Max in 6 Var.c
+++ Control Structure/Nested/Max in 6 Var.c
@@ -1,303 +1,225 @@
 #include<stdio.h>
 
-main()
+main() 
 {
-	int a,b,c,d,e,f;
-	printf("Enter The Value Of A :");
-	scanf("%d",&a);
-	
-	printf("Enter The Value Of B :");
-	scanf("%d",&b);
-	
-	printf("Enter The Value Of C :");
-	scanf("%d",&c);
-	
-	printf("Enter The Value Of D :");
-	scanf("%d",&d);
-	
-	printf("Enter The Value Of E :");
-	scanf("%d",&e);
-	
-	printf("Enter The Value Of F :");
-	scanf("%d",&f);
-	//a,b,c,d,e,f
-	if(a>b)
-	{
-		//a,c,d,e,f
-		if(a>c)
-		{
-			//a,d,e,f
-			if(a>d)
-			{
-				//a,e,f
-				if(a>e)
-				{
-					//a,f
-					if(a>f)
-					{
-						//a
-					printf("A is Max");
-				    }
-				    else
-				    {
-				    	//f
-				    	printf("F is Max");
-					}
-				}
-				else
-				{
-					//e,f
-					if(e>f)
-					{
-						//e
-						printf("E is Max");
-					}
-					else
-					{
-						//f
-						printf("F is Max");
-					}
-				}
-			}
-			else
-			{
-				//d,e,f
-				if(d>e)
-				{
-					//d,f
-					if(d>f)
-					{
-						//d
-						printf("D is Max");
-					}
-					else
-					{
-						printf("F is Max");
-					}
-				}
-				else
-				{
-					//e,f
-					if(e>f)
-					{
-						//e
-						printf("E is Max");
-					}
-					else
-					{
-						//f
-						printf("F is Max");
-					}
-				}
-			}
-		}
-		else
-		{
-			//c,d,e,f
-			if(c>d)
-			{
-				//c,e,f
-				if(c>e)
-				{
-					//c,f
-					if(c>f)
-					{
-						//c
-						printf("C is Max");
-					}
-					else
-					{
-						//f
-						printf("F is Max");
-					}
-				}
-				else
-				{
-					//e,f
-					if(e>f)
-					{
-						//e
-						printf("E is Max");
-					}
-					else
-					{
-						//f
-						printf("F is Max");
-					}
-				}
-			}
-			else
-			{
-				//d,e,f
-				if(d>e)
-				{
-					//d,f
-					if(d>f)
-					{
-						//d
-						printf("D is Max");
-					}
-					else
-					{
-						//f
-						printf("F is Max");
-					}
-				}
-				else
-				{
-					//e,f
-					if(e>f)
-					{
-						//e
-						printf("E is Max");
-					}
-					else
-					{
-						//f
-						printf("F is Max");
-					}
-				}
-			}
-		}
-	}
-	else
-	{
-		//b,c,d,e,f
-		if(b>c)
-		{
-			//b,d,e,f
-			if(b>d)
-			{
-				//b,e,f
-				if(b>e)
-				{
-					//b,f
-					if(b>f)
-					{
-						//b
-						printf("B is Max");	
-					}
-					else
-					{
-						//f
-						printf("F is Max");
-					}
-				}
-				else
-				{
-					//e,f
-					if(e>f)
-					{
-						//e
-						printf("E is Max");
-					}
-					else
-					{
-						//f
-						printf("F is Max");
-					}
-				}
-			}
-			else
-			{
-				//d,e,f
-				if(d>e)
-				{
-					//d,f
-					if(d>f)
-					{
-						//d
-						printf("D is Max");
-					}
-					else
-					{
-						//f
-						printf("F is Max");
-					}
-				}
-				else
-				{
-					//e,f
-					if(e>f)
-					{
-						//e
-						printf("E is Max");
-					}
-					else
-					{
-						//f
-						printf("F is Max");
-					}
-				}
-			}
-		}
-		else
-		{
-			//c,d,e,f
-			if(c>d)
-			{
-				//c,e,f
-				if(c>e)
-				{
-					//c,f
-					if(c>f)
-					{
-						//c
-						printf("C is Max");
-					}
-					else
-					{
-						//f
-						printf("F is Max");						
-					}
-				}
-				else
-				{
-					//e,f
-					if(e>f)
-					{
-						//e
-						printf("E is Max");
-					}
-					else
-					{
-						//f
-						printf("F is Max");
-					}
-				}
-			}
-			else
-			{
-				//d,e,f
-				if(d>e)
-				{
-					//e,f
-					if(e>f)
-					{
-						//e
-						printf("E is Max");
-					}
-					else
-					{
-						//f
-						printf("F is Max");
-					}
-				}
-				else
-				{
-					//e,f
-						if(e>f)
-					{
-						//e
-						printf("E is Max");
-					}
-					else
-					{
-						//f
-						printf("F is Max");
-					}
-				}
-			}
-		}
-	}
+   int a,b,c,d,e,f;
+
+   printf("Enter The Values Of a,b,c,d,e,f : ");
+   scanf("%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f);
+
+   if (a > b) 
+   {
+      if (a > c) 
+      {
+         if (a > d) 
+         {
+            if (a > e) 
+            {
+               if (a > f) 
+               {
+                  printf("A is Max");
+               } 
+               else 
+               {
+                  printf("F is Max");
+               }
+            } 
+            else 
+            {
+               if (e > f) 
+               {
+                  printf("E is Max");
+               } else 
+               {
+                  printf("F is Max");
+               }
+            }
+         } 
+         else 
+         {
+            if (d > e) 
+            {
+               if (d > f) 
+               {
+                  printf("D is Max");
+               } else 
+               {
+                  printf("F is Max");
+               }
+            } 
+            else 
+            {
+               if (e > f) 
+               {
+                   printf("E is Max");
+               } 
+               else 
+               {
+                   printf("F is Max");
+               }
+            }
+         }
+      } 
+      else 
+      {
+         if (c > d) 
+         {
+            if (c > e) 
+            {
+               if (c > f) 
+               {
+                  printf("C is Max");
+               } 
+               else 
+               {
+                   printf("F is Max");
+               }
+            } 
+            else 
+            {
+               if (e > f) 
+               {
+                  printf("E is Max");
+               } 
+               else 
+               {
+                   printf("F is Max");
+               }
+            }
+         } 
+         else 
+         {
+            if (d > e) 
+            {
+               if (d > f) 
+               {
+                   printf("D is Max");
+               } 
+               else 
+               {
+                  printf("F is Max");
+               }
+            } 
+            else
+            {
+               if (e > f) 
+               {
+                  printf("E is Max");
+               } 
+               else 
+               {
+                  printf("F is Max");
+               }
+            }
+         }
+      }
+   } 
+   else 
+   {
+      if (b > c) 
+      {
+         if (b > d)
+         {
+            if (b > e)
+            {
+               if (b > f) 
+               {
+                  printf("B is Max");
+               } 
+               else 
+               {
+                  printf("F is Max");
+               }
+            } 
+            else 
+            {
+               if (e > f)
+               {
+                   printf("E is Max");
+               } 
+               else 
+               {
+                   printf("F is Max");
+               }
+            }
+         } 
+         else 
+         {
+            if (d > e) 
+            {
+               if (d > f) 
+               {
+                   printf("D is Max");
+               } else 
+               {
+                   printf("F is Max");
+               }
+            }
+            else 
+            {
+               if (e > f) 
+               {
+                  printf("E is Max");
+               } 
+               else 
+               {
+                  printf("F is Max");
+               }
+            }
+         }
+      } 
+      else 
+      {
+         if (c > d) 
+         {
+            if (c > e) 
+            {
+               if (c > f) 
+               {
+                   printf("C is Max");
+               }
+               else 
+               {
+                   printf("F is Max");
+               }
+            } 
+            else 
+            {
+               if (e > f)
+               {
+                   printf("E is Max");
+               } 
+               else 
+               {
+                   printf("F is Max");
+               }
+            }
+         }
+         else 
+         {
+            if (d > e) 
+            {
+               if (d > f) 
+               {
+                   printf("D is Max");
+               } 
+               else
+               {
+                   printf("F is Max");
+               }
+            } 
+            else
+            {
+               if (e > f) 
+               {
+                  printf("E is Max");
+               } 
+               else 
+               {
+                  printf("F is Max");
+               }
+            }
+         }
+      }
+   }
 }
