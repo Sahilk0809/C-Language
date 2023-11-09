7c7a01b45431083468160841b8f541ec52741177
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2023-11-09 15:26:58 +0530

    Add files via upload

---
+++ Looping/1 to 10 print.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+	
+	int i=1;
+	
+	while(i<=10)
+	{
+		printf("%d ",i);
+		i++;
+	}
+	
+}
---
+++ Looping/10 to 1 print.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main()
+{
+	
+    int i=10;
+
+    while(i>=1)
+    {
+        printf("%d ", i);
+        i--;
+    }
+    
+}
