ef0f7a402cc51829bea931fe9b51c4d18589b63e
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2023-11-12 23:53:56 +0530

    Add files via upload

---
+++ Looping/Loops_running.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+main() 
+{
+    
+    int count = 0;
+    int limit = 10;
+
+    while (count<limit)
+    {
+        count++;
+    }
+
+    printf("The loop ran %d times\n",count);
+
+}
