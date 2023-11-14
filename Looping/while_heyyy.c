3a5e83e67a7884176bf0924b40f8771005e916b0
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2023-11-14 21:09:09 +0530

    Add files via upload

---
+++ Looping/while_heyyy.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+    int i=1,n;
+    printf("enter the value :");
+    scanf("%d",&n);
+    
+    while(i<=n)
+    {
+        printf("Heyyy...\n");
+        i++;
+    }
+}
