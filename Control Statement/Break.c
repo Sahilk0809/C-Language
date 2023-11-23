ff355cbf5f17a251d23b84ff12d640e8ab754d44
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2023-11-23 15:08:09 +0530

    Add files via upload

---
+++ Control Statement/Break.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main()
+{
+	
+	int i,n;
+	printf("Enter The Limit : ");
+	scanf("%d",&n);
+	
+	for(i=1; i<=10; i++)
+	{
+		printf("%d ",i);
+		if(i==n)
+		{
+			break;
+		}	
+	}
+	
+}
