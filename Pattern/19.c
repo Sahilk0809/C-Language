6515a24721bfb62ff6ee833764d4ac662072badc
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2023-11-28 15:59:23 +0530

    Add files via upload

---
+++ Pattern/19.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+main()
+{
+//1
+//2 3
+//4 5 6
+//7 8 9 10
+//11 12 13 14 15
+	int i,j,k=1;
+    
+    for(i=1; i<=5; i++)
+    {
+        for(j=1; j<=i; j++)
+        {
+        	printf("%d ",k++);
+        }
+        printf("\n");
+    }  
+}
