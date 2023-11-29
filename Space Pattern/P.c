8ae0290bd96ffabe10066d1b5f3fa9bdb9f6aa06
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2023-11-29 19:13:42 +0530

    Add files via upload

---
+++ Space Pattern/P.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+main()
+{
+    int a, b;
+    for(a=1; a<=7; a++)
+    {
+        for(b=1; b<=5; b++)
+        {
+            if((a==1 && b==5)||(a==4 && b==5))
+            {
+                printf("  ");
+            }
+            else if(a==1 || a==4 || b==1 || (a==2 && b==5) || (a==3 && b==5))
+            {
+                printf("* ");
+            }
+            else
+            {
+                printf("  ");        
+            }
+        }
+             printf("\n");
+    }
+
+}
