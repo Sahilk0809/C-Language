b846244a80863054354b8c79fcd03cf6daf5880a
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2023-12-12 02:33:29 -0800

    Add files via upload

---
+++ String/Length_of_the_string.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+#include<String.h>
+
+void main()
+{
+	int n,i,length;//n for string size and i for loop counter and length to store the size of string in which the user input the name
+	printf("Enter the size of String : ");
+	scanf("%d",&n);
+	char s[n];
+	
+	printf("Enter the your name : ");
+	scanf("%s",&s);
+	
+	//process to find the length of string
+	for(i=0; i<s[i]; i++)
+	{
+		length++;
+	}
+	printf("Length of the String : %d",length);
+	
+}
