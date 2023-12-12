b6520c14cd4e4b784f5eb261253741169b9df723
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2023-12-12 02:32:53 -0800

    Add files via upload

---
+++ String/Lowercase_convert.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+#include<String.h>
+
+void main()
+{
+	int n,i;//n for string size and i for loop counter
+	printf("Enter the size of String : ");
+	scanf("%d",&n);
+	char s[n];
+	
+	printf("Enter the your name : ");
+	scanf("%s",&s);
+	
+	//process of converting lowercase
+	for(i=0; i<n; i++)
+	{
+		if(s[i]>=65 && s[i]<=90)
+		{
+			s[i]=s[i]+32;
+		}
+	}
+	printf("Converted into lowercase : %s",s);
+	
+}
