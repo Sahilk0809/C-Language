20da60899a860866a19a1f1e777f94ab9bb4b378
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2023-12-12 02:33:45 -0800

    Add files via upload

---
+++ String/Reverse_String.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+#include<String.h>
+
+void main()
+{
+	int n,i,length;//n for string size and i for loop counter and length to store the size of string in which the user input the name
+	printf("Enter the size of String : ");
+	scanf("%d",&n);
+	char s[n];
+	
+	printf("Enter the your name : ");
+	scanf("%s",&s);
+	
+	for(i=0; i<s[i]; i++)
+	{
+		length++;
+	}
+	//reversing the string
+	for(i=length-1; i>=0; i--)
+	{
+		printf("%c",s[i]);
+	}
+	
+}
