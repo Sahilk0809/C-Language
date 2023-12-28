78f6d8592f77b2d1c1455f6a2dfa81d8670922ee
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2023-12-28 15:35:20 +0530

    Add files via upload

---
+++ UDF Pr/Calculator_endless.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<conio.h>
+
+float add(float x,float y)
+{
+    return x + y;
+}
+float subtract(float x, float y)
+{
+    return x - y;
+}
+float multiply(float x, float y)
+{
+    return x * y;
+}
+float divide(float x, float y)
+{
+    return x / y;
+}
+
+main() 
+{
+    int choice;
+    float num1, num2;
+
+    printf("Welcome to the calculator!\n");
+
+    while (1)
+	 {
+        printf("Please select an operation:\n");
+        printf("1 Add\n");
+        printf("2 Subtract\n");
+        printf("3 Multiply\n");
+        printf("4 Divide\n");
+        printf("0. Exit\n\n");
+
+        printf("Enter the operation : ");
+        scanf("%d", &choice);
+
+        if (choice == 0) 
+        {
+            printf("Thank you for using the calculator!\n");
+            break;
+        }
+
+        printf("Enter the first number: ");
+        scanf("%f", &num1);
+
+        printf("Enter the second number: ");
+        scanf("%f", &num2);
+        printf("\n");
+
+        switch (choice) 
+        {
+            case 1 :
+                printf("Result: %.2f\n\n",add(num1, num2));
+                break;
+            case 2 :
+                printf("Result: %.2f\n\n",subtract(num1, num2));
+                break;
+            case 3 :
+                printf("Result: %.2f\n\n",multiply(num1, num2));
+                break;
+            case 4 :
+                if (num2 != 0) 
+                {
+                    printf("Result: %.2f\n\n",divide(num1, num2));
+                } else 
+                {
+                    printf("Cannot divide by zero\n\n");
+                }
+                break;
+            default:
+                printf("Invalid input. Please try again.\n\n");
+        }
+    }
+}
