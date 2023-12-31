afc4c6597d63e152761588d426dc1be0360f6f90
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2023-12-31 20:17:33 +0530

    Update Cube.c

--- UDF/Cube.c
+++ UDF/Cube.c
@@ -5,12 +5,12 @@ void cube()
 {
     int a;
     printf("Enter the value : ");
-    scanf("%d",&a);
-    a = a * a * a;
+    scanf("%d",&a);//user input
+    a = a * a * a;//cube of the number
     printf("The cube of the value is : %d",a);
 }
 
 void main()
 {
-    cube();
+    cube();//calling function
 }
