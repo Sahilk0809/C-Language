ab480762f577edef12012aaf7e7af9d3502a4582
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2024-01-08 15:44:26 +0530

    Update 31.c

--- Space Pattern/31.c
+++ Space Pattern/31.c
@@ -1,16 +1,22 @@
 #include<stdio.h>
-main()
+#include<conio.h>
+
+void main()
 {
 	int i,j,k=1,y=1,x;
 	for(i=1; i<=10; i++)
 	{
 		for(x=1; x<i; x++)
 		{
-			printf("--");
+			printf(" - ");
 		}
 		for(j=i; j<=10; j++)
 		{
 			printf("%d ",k);
+			if(k>=1 && k<=9)
+			{
+				printf(" ");
+			}
 			k=k+j;
 		}
 		
@@ -24,4 +30,3 @@ main()
 		printf("\n");
 	}
 }
-
